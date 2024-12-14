8e4dd5b66de5a6a8f336a0c1426df0d961e19832
Huy Do <dogiahuy04@gmail.com>
2024-12-14 19:33:18 +0700

    fix: cors error

--- moji-web-main/app.js
+++ moji-web-main/app.js
@@ -43,8 +43,10 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Frontend URL
-    credentials: true, // Allow cookies
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
